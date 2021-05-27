b91110919628adf7d6ab156b983c9f4ac776855b
alonzorozco <sfabian.orozco@gmail.com>
2021-05-26 23:46:48 -0400

    ADD:Desafio 2.0

--- Piendrapapeltijera.rb
+++ Piendrapapeltijera.rb
@@ -18,62 +18,69 @@ puts 'Saliendo...1'
 else
 puts 'Opción inválida'
 end
+
 usuario = opcion_menu
 
 if usuario !="piedra" && usuario !="tijera" && usuario !="papel"
     puts "Argumento invalido: Debe ser piedra, papel o tijera."
 else
 
-# numeros aleatoriosS
-computador = rand(0..2)
+    while opcion_menu != 'salir' && opcion_menu != 'Salir'
+        # Mostrar menú
+        puts 'Turno jugador Dos:'
+        puts '1. Piedra'
+        puts '2. Papel'
+        puts '3. Tijeras'
+        puts '2. Salir'
+        opcion_menu = gets.chomp
+        if opcion_menu == 'Piedra'
+        puts 'Realizando acción 1...'
+        elsif opcion_menu == 'Papel'
+        puts 'Realizando acción 2...'
+        elsif opcion_menu == 'Tijera'
+            puts 'Realizando acción 3...'
+        elsif opcion_menu == 'salir' || opcion_menu == 'Salir'
+        puts 'Saliendo...1'
+        else
+        puts 'Opción inválida'
+        end
 
-    if computador == 0 
-    computador="piedra"
-    end
-
-    if computador == 1
-    computador ="papel"
-    end
-
-    if computador == 2
-    computador= "tijera"
-    end
-
-# mensaje de computador
-    puts "computador juega #{computador}"
-
-
+        usuario2 = opcion_menu
+    if usuario2 !="piedra" && usuario2 !="tijera" && usuario2 !="papel"
+            puts "Argumento invalido: Debe ser piedra, papel o tijera."
+    else
 
 #  resultado de juego
-    if computador == usuario
+    if usuario2  == usuario
         puts "empataste"
     
 
-    elsif computador =="piedra" and usuario =="papel"
+    elsif usuario2  =="piedra" and usuario =="papel"
             puts "ganaste"
 
-    elsif computador =="piedra" and usuario =="tijera"
+    elsif usuario2  =="piedra" and usuario =="tijera"
             puts "pediste"
 
 
-    elsif computador =="papel" and usuario =="tijera"
+    elsif usuario2  =="papel" and usuario =="tijera"
             puts "ganaste"
 
-    elsif computador =="papel" and usuario =="piedra"
+    elsif usuario2  =="papel" and usuario =="piedra"
             puts "perdiste"
 
-    elsif computador =="tijera" and usuario =="piedra"
+    elsif usuario2  =="tijera" and usuario =="piedra"
             puts "ganaste"
             
     else 
-        computador =="tijera" and usuario =="papel"
+        usuario2 =="tijera" and usuario =="papel"
             puts "perdiste"
     
     end
 end
 
 
-end
+
+
 
 
 
